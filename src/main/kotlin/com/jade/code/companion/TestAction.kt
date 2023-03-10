5a07c9e3d4be2a65d96bccd650905fad9a7ec3a1
BeauteousJade <pby2196995023@163.com>
2023-03-11 02:16:23 +0800

    新增test action

---
+++ src/main/kotlin/com/jade/code/companion/TestAction.kt
@@ -0,0 +1,10 @@
+package com.jade.code.companion
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class TestAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+}
